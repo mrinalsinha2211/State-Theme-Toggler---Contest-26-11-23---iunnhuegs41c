c4a0d34f5030ab311466ea2da983016284229e5a
mrinalsinha2211 <145749474+mrinalsinha2211@users.noreply.github.com>
2023-11-26 19:28:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,38 @@
 import React, { useState } from "react";
 import "../styles/App.css";
-
 export default function App() {
-    return (
-        <div className="App">
-            <h1>Newton School</h1>
-            <div>
-                <form>
-                    <label>UserName</label>
-                    <input></input>
-                    <label>Password</label>
-                    <input></input>
-                    <button>Login</button>
-                </form>
-            </div>
-            <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
-            <input type="checkbox" id="switch" />
-            <label for="switch" className="label">
-                Toggle
-            </label>
-        </div>
-    );
+  // Step 1: Initialize state for theme
+  const [theme, setTheme] = useState("light");
+
+  // Step 2: Apply conditional styling based on the theme
+
+  // Step 3: Toggle theme function
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <div className='App' id={theme === "light" ? "light" : "dark"}>
+      <h1>Newton School</h1>
+      <div>
+        <form>
+          <label>UserName</label>
+          <input />
+          <label>Password</label>
+          <input />
+          <button>Login</button>
+        </form>
+      </div>
+      <label>{theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+      <input
+        type="checkbox"
+        id="switch"
+        onChange={toggleTheme}
+        checked={theme === "dark"}
+      />
+      <label htmlFor="switch" className="label">
+        Toggle
+      </label>
+    </div>
+  );
 }
